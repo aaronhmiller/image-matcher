1326b7862f8b11080d9a70c4b1f5c1b0addac267
Aaron Miller <aaron.miller@chainguard.dev>
2025-08-14 21:57:34 -0700

    keeping original name for better post-processing

--- process-matched_images.sh
+++ process-matched_images.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 awk -F',' '{print $1}' matched_images.csv > originallist-input.txt
-awk -F',' '{print "cgr.dev/chainguard/" $2}' matched_images.csv > newlist-input.txt
+awk -F',' '{print "cgr.dev/chainguard-private/" $3}' matched_images.csv > newlist-input.txt
 # Detect GNU vs BSD sed and trim header row
 if sed --version 2>/dev/null | grep -q GNU; then
     sed -i '1d' originallist-input.txt
