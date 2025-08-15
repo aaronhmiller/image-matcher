1ee72d2c6bbeb9f7f02b89de3a392b04576d14d4
Aaron Miller <aaron.miller@chainguard.dev>
2025-08-14 19:45:19 -0700

    adding processor

---
+++ process-matched_images.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+awk -F',' '{print $1}' matched_images.csv > originallist-input.txt
+awk -F',' '{print "cgr.dev/chainguard/" $2}' matched_images.csv > newlist-input.txt
+# Detect GNU vs BSD sed and trim header row
+if sed --version 2>/dev/null | grep -q GNU; then
+    sed -i '1d' originallist-input.txt
+    sed -i '1d' newlist-input.txt
+else
+    sed -i '' '1d' originallist-input.txt
+    sed -i '' '1d' newlist-input.txt
+fi
